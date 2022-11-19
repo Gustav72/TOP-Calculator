49e23d2529ef6b165636e545ca4556a01d3136b4
Wolfgang <wolfgangkirstein7@gmail.com>
2022-11-18 22:15:47 -0500

    Add operator functionality

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let a;
 let b;
-let c;
+let operator;
+
 
 function addIt(a, b) {
     return a + b;
 }
 
 function subtractIt(a, b) {
-    return a - b;
+    return  a - b;
 }
 
 function multiplyIt(a, b) {
@@ -23,44 +24,43 @@ function percentIt(a) {
 }
 
 function operate(operator, a, b) {
-    if (operator == add) return add(a, b);
-    else if (operator == subtract) return subtract(a, b);
-    else if (operator == multiply) return multiply(a, b);
-    else if (operator == divide) return divide(a, b);
-    else if (operator == percent) return percent(a);
-    else return 'what are you doing bro?!?!?!?!'
+    if (operator == 'add') display.textContent = addIt(a, b);
+    else if (operator == 'subtract') display.textContent = subtractIt(a, b);
+    else if (operator == 'multiply') display.textContent = multiplyIt(a, b);
+    else if (operator == 'divide' && b == 0) display.textContent = "DON\'T BREAK IT";
+    else if (operator == 'divide') display.textContent = divideIt(a, b);
+    else if (operator == 'percent') display.textContent = percentIt(a);
+    else alert('what are you doing bro?!?!?!?!');
 }
 
 const calculator = document.getElementById('main');
 
 const display = document.getElementById('display');
-display.textContent = '';
 
 const allClear = document.querySelector('#all-clear');
 const backspace = document.querySelector('#backspace');
+
 const percent = document.querySelector('#percent');
 const plus = document.querySelector('#plus');
-const seven = document.getElementById('seven');
-const eight = document.querySelector('#eight');
-const nine = document.querySelector('#nine');
 const minus = document.querySelector('#minus');
-const four = document.querySelector('#four');
-const five = document.querySelector('#five');
-const six = document.querySelector('#six');
 const multiply = document.querySelector('#multiply');
+const divide = document.querySelector('#divide');
+
+const sign = document.querySelector('#sign');
+const decimal = document.querySelector('#decimal');
+
+const equal = document.querySelector('#equal');
+
+const zero = document.querySelector('#zero');
 const one = document.querySelector('#one');
 const two = document.querySelector('#two');
 const three = document.querySelector('#three');
-const divide = document.querySelector('#divide');
-const sign = document.querySelector('#sign');
-const zero = document.querySelector('#zero');
-const decimal = document.querySelector('#decimal');
-const equal = document.querySelector('#equal');
-
-
-
-
-
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.getElementById('seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
 
 zero.onclick = () => display.textContent += '0';
 one.onclick = () => display.textContent += '1';
@@ -71,14 +71,80 @@ five.onclick = () => display.textContent += '5';
 six.onclick = () => display.textContent += '6';
 seven.onclick = () => display.textContent += '7';
 eight.onclick = () => display.textContent += '8';
-seven.onclick = () => display.textContent += '9';
-decimal.onclick = () => display.textContent += '.';
+nine.onclick = () => display.textContent += '9';
+
+plus.onclick = () => {
+   a = Number(display.textContent);
+   display.textContent = '';
+   console.log(a);
+   operator = 'add';
+
+}
+
+minus.onclick = () => {
+    a = Number(display.textContent);
+    display.textContent = '';
+    console.log(a);
+    operator = 'subtract';
+
+ }
+
+ multiply.onclick = () => {
+    a = Number(display.textContent);
+    display.textContent = '';
+    console.log(a);
+    operator = 'multiply';
+
+ }
+
+ divide.onclick = () => {
+    a = Number(display.textContent);
+    display.textContent = '';
+    console.log(a);
+    operator = 'divide';
+
+ }
+
+ divide.onclick = () => {
+    a = Number(display.textContent);
+    display.textContent = '';
+    console.log(a);
+    operator = 'divide';
+
+ }
+
+ percent.onclick = () => {
+    a = Number(display.textContent);
+    display.textContent = '';
+    operator = 'percent';
+    operate(operator, a, b)
+
+
+ }
+
+decimal.onclick = () => {
+    if (display.textContent.includes('.')) return
+    else display.textContent += '.';
+}
+
+allClear.onclick = () => {
+    display.textContent = '';
+    
+}
+
+backspace.onclick = () => {
+    display.textContent = display.textContent.slice(0, -1);
+}
+
+sign.onclick = () => {
+    if (!display.textContent.includes('-')) display.textContent = '-' + display.textContent
+    else if (display.textContent.includes('-')) display.textContent = display.textContent.substring(1);
+}
 
 equal.onclick = function() {
 
-display.textContent += '';
-a = display.textContent;
-console.log(a); 
+b = Number(display.textContent);
+operate(operator, a, b)
 
 }
 
